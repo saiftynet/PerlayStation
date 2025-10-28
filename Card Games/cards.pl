f6091dc107d6185aaf0589a26879da35708075f9
Saif Ahmed <34284663+saiftynet@users.noreply.github.com>
2025-10-28 16:18:03 +0000

    Refactor card handling and update display usage

--- Card Games/cards.pl
+++ Card Games/cards.pl
@@ -2,15 +2,15 @@
 use strict;use warnings;
 use Object::Pad;
 use lib "./lib/";
-use Display;
-use Cards; 
+use Games::PerlayStation::Display;
+use Games::PerlayStation::Cards; 
 use utf8;
 binmode(STDOUT, ":utf8");
 my $d=new Display();
 
 
  my $deck=new Stack;
- $deck->fullDeck();#
+ $deck->fullDeck();
  my $player1=new Stack;
  
  my $card=new Card("Ace Of Spades");
@@ -22,10 +22,10 @@ my $d=new Display();
  $card->medium($d);
  $card=new Card({number=>"9",suit=>"hearts"},[5,9]);
  $card->medium($d);
- $card=new Card({number=>"8",suit=>"clubs"},[6,10]);
+ $card=new Card({number=>"8",suit=>"clubs",pos=>[6,10]});
  $card->flip();
  $card->medium($d);
  $card=$deck->shuffle()->deal([7,11]);
- $card->set_face("down");
+ $card->set_face("up");
  $card->large($d);
  $deck->shuffle()->deal($player1);
